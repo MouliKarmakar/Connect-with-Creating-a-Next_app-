bd960a2fb0d2a07bd1bb87e19883570121bad255
MouliKarmakar <moulikarmakar7596@gmail.com>
2024-08-25 14:56:44 +0530

    implemented cleark auth pages

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/SignUpPage.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+export default function SignUpPage() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <SignUp />
+    </main>
+  );
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SignUpPage from "./SignUpPage";
+
+export default function Page() {
+  return <SignUpPage />;
+}
